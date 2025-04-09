6fb14fd2998ed300ecf003c6e7713d40b38532b6
PerniceSimone <simone.pernice@unito.it>
2025-04-09 16:06:02 +0200

    SP: starting the folder organisation

---
+++ Main.R
@@ -0,0 +1,8 @@
+
+# Loading all the necessary 
+source("Settings/utilities.R")
+source("Settings/Packages.R")
+source("Settings/Colorpalette.R")
+
+# Step 0: Loading the data and metadata
+source("Workflow/Step0_DataImport/DataImport.R")
