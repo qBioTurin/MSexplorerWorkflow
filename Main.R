7f29711af53b2900987df865e50b4c56e8701539
franky2204 <fpantini1997@gmail.com>
2025-04-10 12:16:50 +0200

    update Main

--- Main.R
+++ Main.R
@@ -4,5 +4,38 @@ source("Settings/utilities.R")
 source("Settings/Packages.R")
 source("Settings/Colorpalette.R")
 
+
 # Step 0: Loading the data and metadata
 source("Workflow/Step0_DataImport/DataImport.R")
+
+# Step 1: use Deseq to do unsupervised decontamination
+source("Workflow/Step1_DESEQ/DESEQ.R")
+
+# Step 2: use supervised decontamination
+source("Workflow/Step2_Decontamination/Supervised_decontam.R")
+
+# Step 3: create graphs of Alpha, Beta diversity and Stackbars
+#Alpha
+source("Workflow/Step3_Graphs/Alpha.R") 
+#Beta
+source("Workflow/Step3_Graphs/Beta.R")
+#Stackbars
+source("Workflow/Step3_Graphs/Stack.R")
+
+# Step 4: creation of DAS using limma and lefse
+#limma
+source("Workflow/Step4_DiscriminantAnalysis/Limma/DAS_LIMMA.R")
+#lefse
+source("Workflow/Step4_DiscriminantAnalysis/Lefse/PreLefse.R")
+#furthermore for lefse you need other steps see README
+#fuse DAS
+source("Workflow/Step4_DiscriminantAnalysis/Merge_DAS/Merge_DAS.R")
+
+# Step 5: create of heatmaps
+source("Workflow/Step5_Heatmaps/HeatmapNew.R")
+
+# Step 6 calculate Alpha diversity DAS
+source("Workflow/Step6_DAS_Alpha/DAS_ALPHA.R")
+
+# Step 7: LEAPS?
+source("Workflow/Step7_Leaps/script_LEAPS_nuovo_perpaper.R")
